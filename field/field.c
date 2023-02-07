d19291eaa889930d2a53c16d091432601f1b07a7
mchiozzi <mchiozzi95@gmail.com>
2023-02-07 17:29:59 -0300

    create field library

---
+++ field/field.c
@@ -0,0 +1,23 @@
+#include "field.h"
+
+#include "math.h"
+
+void setCoordinate(Coordinate *point, float x, float y, float z) {
+    point->x = x;
+    point->y = y;
+    point->z = z;
+}
+
+void setVector(Vector *vector, float x, float y, float z) {
+    vector->x = x;
+    vector->y = y;
+    vector->z = z;
+}
+
+float euclidean_distance2(Coordinate a, Coordinate b) {
+    float delta_x = a.x - b.x;
+    float delta_y = a.y - b.y;
+    float delta_z = a.z - b.z;
+
+    return delta_x * delta_x + delta_y * delta_y + delta_z * delta_z;
+}
---
+++ field/field.h
@@ -0,0 +1,25 @@
+#ifndef FIELD_H
+#define FIELD_H
+
+// defines a spacial position
+typedef struct
+{
+    float x;
+    float y;
+    float z;
+} Coordinate;
+
+// defines a vector
+typedef struct
+{
+    float x;
+    float y;
+    float z;
+} Vector;
+
+void setCoordinate(Coordinate *point, float x, float y, float z);
+void setVector(Vector *vector, float x, float y, float z);
+
+float euclidean_distance2(Coordinate point_a, Coordinate point_b);
+
+#endif
