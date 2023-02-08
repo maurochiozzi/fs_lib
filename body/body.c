851f1b45bb6d176d55de447029675602d7862ad6
mchiozzi <mchiozzi95@gmail.com>
2023-02-08 18:22:27 -0300

    small fixies

--- body/body.c
+++ body/body.c
@@ -5,8 +5,6 @@
 void initBody(Body *body) {
     setCoordinate(&body->position, 0.0, 0.0, 0.0);
     setVector(&body->heading, 0.0, 0.0, 0.0);
-
-    return 0;
 }
 
 void updatePosition(Body *body, Coordinate position, Vector heading) {
--- main.c
+++ main.c
@@ -1,5 +1,5 @@
-#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "beacon/beacon.h"
 #include "body/body.h"
@@ -8,7 +8,8 @@
 #define PREVIOUS_POSITIONS_SIZE 10
 
 Body body;
-Body previous_positions[PREVIOUS_POSITIONS_SIZE];
+Body *previous_positions;
+int previous_position_index;
 
 // stores beacons frequency, magnetic moment and location
 #define MAX_REACHABLE_BEACONS 5
@@ -21,12 +22,13 @@ Beacon *beacons;
 Coordinate *environment;
 
 // stores magnetic space samples
-#define MAGNETIC_space_SAMPLE_SIZE ;
+#define MAGNETIC_INTENSITY_SAMPLE_SIZE ;
 
-short magnetic_space_sample_index;
-Vector **magnetic_space_samples;
+int magnetic_intensity_samples;
+Vector **magnetic_intensity_samples;
 
 int init();
+int initPreviousPositionArray();
 int initBeacons();
 int initEnvironments();
 
@@ -54,7 +56,13 @@ int main(void) {
 }
 
 int init() {
+    printf("Initializing...\n");
+
     initBody(&body);
+
+    printf("Body position (%.2f, %.2f, %.2f)\n", body.heading.x, body.heading.y, body.heading.z);
+
+    initPreviousPositionArray();
     initBeacons();
     initEnvironments();
     initSensors();
@@ -62,11 +70,23 @@ int init() {
     return 0;
 }
 
+int initPreviousPositionArray() {
+    previous_position_index = 0;
+
+    previous_positions = (Body *)malloc(PREVIOUS_POSITIONS_SIZE);
+
+    return 0;
+}
+
 int initBeacons() {
+    beacons = (Beacon *)malloc(MAX_REACHABLE_BEACONS);
+
     return 0;
 }
 
 int initEnvironments() {
+    environment = (Coordinate *)malloc(MAX_ENVIRONMENT_NODES);
+
     return 0;
 }
 
@@ -75,6 +95,8 @@ int initSensors() {
 }
 
 int fetchData(Beacon *beacons, Coordinate *environment) {
+    printf("Fetching data...\n");
+
     // fetch beacon informations (frequency and magnetic moment) from station
     fetchBeaconsData(beacons);
 
@@ -93,13 +115,17 @@ int fetchEnvironmentBoundaries(Coordinate *environment) {
 }
 
 int surveyBeacons(Beacon *beacons, Coordinate *environment) {
+    printf("Surveying beacons...\n");
+
     return 0;
 }
 
 int acquireBodyPosition() {
+    printf("Acquiring position...\n");
+
     while (1) {
-        // while (magnetic_space_sample_index < MAGNETIC_space_SAMPLE_SIZE) {
-        //     sample_magnetic_space(&magnetic_space_samples, magnetic_space_sample_index);
+        // while (magnetic_instensity_samples < MAGNETIC_SAMPLE_SIZE) {
+        //     sample_magnetic_space(&magnetic_space_samples, magnetic_instensity_samples);
 
         //     update_magnetic_space_spectrum(&magnetic_space_samples);
         // }
