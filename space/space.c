e4da0691f1972c0b0597e1acb168e0b89987ccdd
mchiozzi <mchiozzi95@gmail.com>
2023-03-31 22:53:53 -0300

    add comments

--- space/space.c
+++ space/space.c
@@ -1,25 +1,60 @@
+/**
+ * @file space.c
+ * @brief Implementation of functions for working with 3D coordinates and vectors
+ *
+ * @see space.h
+ */
+
 #include "space.h"
 
+#include <math.h>
 #include <stdio.h>
 
-#include "math.h"
-
+/**
+ * @brief Sets the x, y, and z coordinates of a Coordinate struct
+ *
+ * @param point A pointer to the Coordinate struct to be set
+ * @param x The x-coordinate to set
+ * @param y The y-coordinate to set
+ * @param z The z-coordinate to set
+ */
 void setCoordinate(Coordinate *point, float x, float y, float z) {
     point->x = x;
     point->y = y;
     point->z = z;
 }
 
+/**
+ * @brief Sets the x, y, and z components of a Vector struct
+ *
+ * @param vector A pointer to the Vector struct to be set
+ * @param x The x-component to set
+ * @param y The y-component to set
+ * @param z The z-component to set
+ */
 void setVector(Vector *vector, float x, float y, float z) {
     vector->x = x;
     vector->y = y;
     vector->z = z;
 }
 
+/**
+ * @brief Calculates the norm (magnitude) of a Vector struct
+ *
+ * @param vector The Vector struct to calculate the norm of
+ * @return The magnitude of the vector
+ */
 float norm(Vector vector) {
     return sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
 }
 
+/**
+ * @brief Calculates the squared Euclidean distance between two Coordinate structs
+ *
+ * @param start A pointer to the starting Coordinate struct
+ * @param end A pointer to the ending Coordinate struct
+ * @return The squared Euclidean distance between start and end
+ */
 float euclideanDistance2(Coordinate *start, Coordinate *end) {
     float delta_x = start->x - end->x;
     float delta_y = start->y - end->y;
@@ -28,12 +63,26 @@ float euclideanDistance2(Coordinate *start, Coordinate *end) {
     return sqrt(delta_x * delta_x + delta_y * delta_y + delta_z * delta_z);
 }
 
+/**
+ * @brief Adds two Coordinate structs together and stores the result in a third Coordinate struct
+ *
+ * @param point A pointer to the first Coordinate struct to be added
+ * @param offset A pointer to the second Coordinate struct to be added
+ * @param result A pointer to the Coordinate struct to store the result in
+ */
 void sumCoordinateOffset(Coordinate *point, Coordinate *offset, Coordinate *result) {
     result->x = point->x + offset->x;
     result->y = point->y + offset->y;
     result->z = point->z + offset->z;
 }
 
+/**
+ * @brief Calculates the position error between two Coordinate structs
+ *
+ * @param reference A pointer to the reference Coordinate struct
+ * @param real A pointer to the real Coordinate struct
+ * @return The position error between reference and real
+ */
 float calculatePositionError(Coordinate *reference, Coordinate *real) {
     Vector error;
 
@@ -44,6 +93,11 @@ float calculatePositionError(Coordinate *reference, Coordinate *real) {
     return norm(error);
 }
 
+/**
+ * @brief Prints the x, y, and z coordinates of a Coordinate struct to stdout
+ *
+ * @param point A pointer to the Coordinate struct to be printed
+ */
 void printCoordinate(Coordinate *point) {
     printf("(%3.6f, %3.6f, %3.6f)\n", point->x, point->y, point->z);
 }
--- space/space.h
+++ space/space.h
@@ -1,43 +1,108 @@
+/**
+ * @file space.h
+ * @brief Defines data structures and functions related to spacial coordinates and vectors.
+ */
+
 #ifndef SPACE_H
 #define SPACE_H
 
-// defines a spacial position
+/**
+ * @brief Represents a point in a 3D space.
+ */
 typedef struct Coordinate {
-    float x;
-    float y;
-    float z;
+    float x; /**< The x-coordinate of the point. */
+    float y; /**< The y-coordinate of the point. */
+    float z; /**< The z-coordinate of the point. */
 } Coordinate;
 
-// defines a vector
+/**
+ * @brief Represents a 3D vector.
+ */
 typedef struct Vector {
-    float x;
-    float y;
-    float z;
+    float x; /**< The x-component of the vector. */
+    float y; /**< The y-component of the vector. */
+    float z; /**< The z-component of the vector. */
 } Vector;
 
+/**
+ * @brief Represents a line segment in a 3D space.
+ */
+typedef struct Segment {
+    Coordinate reference; /**< The starting point of the line segment. */
+    float magnitude;      /**< The magnitude (length) of the line segment. */
+} Segment;
+
+/**
+ * @brief Represents a line segment defined by its two endpoints.
+ */
 typedef struct Baseline {
-    Coordinate initial_point;
-    Coordinate ending_point;
+    Coordinate initial_point; /**< The starting point of the line segment. */
+    Coordinate ending_point;  /**< The ending point of the line segment. */
 } Baseline;
 
 /**
- * A segment defined by its origin and magnitude
+ * @brief Sets the coordinates of a point.
+ *
+ * @param point Pointer to the point whose coordinates are being set.
+ * @param x The x-coordinate of the point.
+ * @param y The y-coordinate of the point.
+ * @param z The z-coordinate of the point.
  */
-typedef struct Segment {
-    Coordinate reference;
-
-    float magnitude;
-} Segment;
-
 void setCoordinate(Coordinate *point, float x, float y, float z);
+
+/**
+ * @brief Sets the components of a vector.
+ *
+ * @param vector Pointer to the vector whose components are being set.
+ * @param x The x-component of the vector.
+ * @param y The y-component of the vector.
+ * @param z The z-component of the vector.
+ */
 void setVector(Vector *vector, float x, float y, float z);
 
+/**
+ * @brief Calculates the Euclidean norm of a vector.
+ *
+ * @param vector The vector whose norm is being calculated.
+ *
+ * @return The Euclidean norm of the vector.
+ */
 float norm(Vector vector);
+
+/**
+ * @brief Calculates the Euclidean distance between two points.
+ *
+ * @param start Pointer to the starting point.
+ * @param end Pointer to the ending point.
+ *
+ * @return The Euclidean distance between the two points.
+ */
 float euclideanDistance2(Coordinate *start, Coordinate *end);
+
+/**
+ * @brief Calculates the result of adding an offset to a point.
+ *
+ * @param point Pointer to the point being offset.
+ * @param offset Pointer to the offset to be added to the point.
+ * @param result Pointer to the result of the addition.
+ */
 void sumCoordinateOffset(Coordinate *point, Coordinate *offset, Coordinate *result);
 
+/**
+ * @brief Calculates the position error between a reference point and a real point.
+ *
+ * @param reference Pointer to the reference point.
+ * @param real Pointer to the real point.
+ *
+ * @return The position error between the two points.
+ */
 float calculatePositionError(Coordinate *reference, Coordinate *real);
 
+/**
+ * @brief Prints the coordinates of a point to the console.
+ *
+ * @param point Pointer to the point being printed.
+ */
 void printCoordinate(Coordinate *point);
 
 #endif
